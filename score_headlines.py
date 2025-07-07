ac0ebf53488edfeede0ef42c95ded6b22d56efe9
Devyani Mahajan <dmahaja1@swarthmore.edu>
2025-07-07 10:40:35 -0500

    made file

---
+++ score_headlines.py
@@ -0,0 +1,13 @@
+#Author: Devi Mahajan
+#Date: 7.7.25
+
+"""
+Create a program, called score_headlines.py
+This program should accept an input text file which will contain one headline per line
+This program should also accept a parameter describing the source of the headlines (example: chicagotribune, nyt)
+If the client does not provide these two parameters, the program should give an appropriate and friendly error message
+This program should output a new file, called headline_scores_source_year_month_day.txt (example: headline_scores_nyt_2025_01_15.txt)
+"""
+
+# imports
+

a631e0f9144cd696e63aecb711e16dbf99358434
Devyani Mahajan <dmahaja1@swarthmore.edu>
2025-07-22 16:39:10 -0500

    ran black

--- score_headlines_api.py
+++ score_headlines_api.py
@@ -2,11 +2,11 @@
 # Date: 22.7.25
 # Port to use: 8012
 
-#You have already deployed the headline sentiment analysis model as a batch job. 
-#Your task now is to deploy it as a web service so clients can make real-time requests.
+# You have already deployed the headline sentiment analysis model as a batch job.
+# Your task now is to deploy it as a web service so clients can make real-time requests.
 
-#Note: the environment needed for this differs from that of score_headlines.py
-#Use apienv.yml 
+# Note: the environment needed for this differs from that of score_headlines.py
+# Use apienv.yml
 
 # IMPORTS
 import logging
@@ -36,20 +36,23 @@ except Exception as e:
     log.error(f"Startup failure: could not load model or encoder. {e}")
     raise RuntimeError(f"Startup failed: {e}") from e
 
+
 class HeadlinePayload(BaseModel):
-    #Expected input format: list of headlines
+    # Expected input format: list of headlines
     headlines: List[str]
+
 
 # LOGS
 @app.get("/healthcheck")
 def healthcheck():
-    #Basic GET endpoint to verify the API is up
+    # Basic GET endpoint to verify the API is up
     log.info("Healthcheck ping received.")
     return {"status": "ok"}
 
+
 @app.post("/analyze")
 def analyze_headlines(payload: HeadlinePayload):
-    #Score list of headlines using encoder + SVM model
+    # Score list of headlines using encoder + SVM model
     log.info("Received headline scoring request.")
 
     if not payload.headlines:
@@ -63,7 +66,9 @@ def analyze_headlines(payload: HeadlinePayload):
         return {"predictions": predictions.tolist()}
     except Exception as e:
         log.error(f"Prediction error: {e}")
-        raise HTTPException(status_code=500, detail=f"Model failed to make predictions. {e}") from e
+        raise HTTPException(
+            status_code=500, detail=f"Model failed to make predictions. {e}"
+        ) from e
 
 
 # RUN
